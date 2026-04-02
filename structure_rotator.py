5420b459e7407f78d0407ee61ec3cde8f87df25a
vo2075 <167514474+vo2075@users.noreply.github.com>
2026-04-02 10:44:28 +0200

    change rotation script
    not sure if it works

--- structure_rotator.py
+++ structure_rotator.py
@@ -6,92 +6,106 @@ from multiprocessing import Pool
 
 directory = 'src/main/resources/data/ohthetreesyoullgrow/structures/features/trees'
 
-def rotate_block_state(state):
+def rotate_block_state(state, angle):
     directions = ['north', 'east', 'south', 'west']
-    # Maak een diepe kopie van de properties om bleeding te voorkomen
-    props = {k: v for k, v in state.items()}
-    
-    # 1. Facing
+    props = dict(state) # Maak een kopie
+    steps = (angle // 90) % 4
+
+    # 1. Rotate 'facing' (stairs, observers, chests, etc.)
     if 'facing' in props:
         val = str(props['facing']).strip("'\"")
         if val in directions:
-            props['facing'] = String(directions[(directions.index(val) + 1) % 4])
-    
-    # 2. Axis
-    if 'axis' in props:
+            props['facing'] = String(directions[(directions.index(val) + steps) % 4])
+
+    # 2. Rotate 'axis' (logs, quartz pillars)
+    if 'axis' in props and steps % 2 != 0:
         val = str(props['axis']).strip("'\"")
         if val == 'x': props['axis'] = String('z')
         elif val == 'z': props['axis'] = String('x')
 
-    # 3. Connections (Fences, Walls, Leaves)
-    # Belangrijk: we lezen uit 'state' en schrijven naar 'props'
-    for i, d in enumerate(directions):
-        if d in state:
-            new_dir = directions[(i + 1) % 4]
-            props[new_dir] = state[d]
-            
+    # 3. Rotate connections (fences, walls, glass panes)
+    # Maak een tijdelijke map om botsingen te voorkomen
+    connection_changes = {}
+    found_any = False
+    for d in directions:
+        if d in props:
+            found_any = True
+            new_dir = directions[(directions.index(d) + steps) % 4]
+            connection_changes[new_dir] = props[d]
+    
+    if found_any:
+        # Verwijder oude richtingen
+        for d in directions:
+            props.pop(d, None)
+        # Voeg nieuwe toe
+        props.update(connection_changes)
+
     return Compound(props)
 
-def rotate_structure(nbt_data):
-    # We halen de data op
+def rotate_structure(nbt_data, angle):
     old_size = nbt_data['size']
-    old_x, old_y, old_z = int(old_size[0]), int(old_size[1]), int(old_size[2])
-    
-    # 1. Palette veilig roteren
-    # We maken een compleet nieuwe lijst om ID-verwarring te voorkomen
+    sx, sy, sz = int(old_size[0]), int(old_size[1]), int(old_size[2])
+    steps = (angle // 90) % 4
+
+    # 1. Rotate Palette
     new_palette = List[Compound]()
     for entry in nbt_data['palette']:
         new_entry = Compound({'Name': entry['Name']})
         if 'Properties' in entry:
-            new_entry['Properties'] = rotate_block_state(entry['Properties'])
+            new_entry['Properties'] = rotate_block_state(entry['Properties'], angle)
         new_palette.append(new_entry)
 
-    # 2. Blokken roteren
+    # 2. Rotate Blocks & Size
     new_blocks = List[Compound]()
+    
+    # Bepaal nieuwe size
+    if steps % 2 == 0:
+        nsx, nsy, nsz = sx, sy, sz
+    else:
+        nsx, nsy, nsz = sz, sy, sx
+
     for block in nbt_data['blocks']:
-        bx, by, bz = map(int, block['pos'])
+        x, y, z = map(int, block['pos'])
         
-        # Rotatie formule
-        new_pos = List[Int]([Int((old_z - 1) - bz), Int(by), Int(bx)])
-        
+        if steps == 1: # 90 degrees clockwise
+            nx, nz = (sz - 1) - z, x
+        elif steps == 2: # 180 degrees
+            nx, nz = (sx - 1) - x, (sz - 1) - z
+        elif steps == 3: # 270 degrees clockwise
+            nx, nz = z, (sx - 1) - x
+        else:
+            nx, nz = x, z
+
         new_block = Compound({
-            'pos': new_pos,
-            'state': block['state'] # De index blijft hetzelfde
+            'pos': List[Int]([Int(nx), Int(y), Int(nz)]),
+            'state': block['state']
         })
         if 'nbt' in block:
             new_block['nbt'] = copy.deepcopy(block['nbt'])
         new_blocks.append(new_block)
 
-    # Bouw een schone nieuwe root op
-    new_root = Compound({
-        'size': List[Int]([Int(old_z), Int(old_y), Int(old_x)]),
+    return Compound({
+        'size': List[Int]([Int(nsx), Int(nsy), Int(nsz)]),
         'palette': new_palette,
         'blocks': new_blocks,
-        'DataVersion': nbt_data.get('DataVersion', Int(2584))
+        'DataVersion': nbt_data.get('DataVersion', Int(3465))
     })
-    return new_root
 
 def process_file(file_info):
     root_dir, file = file_info
     path = os.path.join(root_dir, file)
     try:
-        # Laad het bestand
+        # Laad het bronbestand één keer
         nbt_file = nbtlib.load(path)
-        
-        # De 'nbt_file' zelf is vaak al de root compound.
-        # We pakken de data en zorgen dat het een Compound is voor onze rotate functie.
-        current_root = Compound(nbt_file)
+        base_root = Compound(nbt_file)
         
         for angle in [90, 180, 270]:
-            # Roteer de data
-            current_root = rotate_structure(current_root)
+            rotated_root = rotate_structure(base_root, angle)
             
             new_name = file.replace('.nbt', f'_{angle}.nbt')
             new_path = os.path.join(root_dir, new_name)
             
-            # Opslaan: We maken een nieuwe File wrapper om de Compound heen.
-            # We gebruiken gzipped=True omdat Minecraft structures dit vereisen.
-            output_file = nbtlib.File(current_root, gzipped=True)
+            output_file = nbtlib.File(rotated_root, gzipped=True)
             output_file.save(new_path)
             
         return f"✅ Succes: {file}"
@@ -102,10 +116,15 @@ if __name__ == '__main__':
     tasks = []
     for root_dir, _, files in os.walk(directory):
         for f in files:
-            if f.endswith('.nbt') and not any(x in f for x in ['_90', '_180', '_270'])and not f.startswith('blank_trunk.nbt'):
-                tasks.append((root_dir, f))
+            # Pak alleen originele bestanden
+            if f.endswith('.nbt') and not any(x in f for x in ['_90', '_180', '_270']):
+                if not f.startswith('blank_trunk'):
+                    tasks.append((root_dir, f))
 
     print(f"Start rotatie van {len(tasks)} bomen...")
     with Pool() as pool:
         results = pool.map(process_file, tasks)
-        for r in results: print(r)
+        # Optioneel: alleen fouten printen om de console schoon te houden
+        for r in results:
+            if "❌" in r: print(r)
+    print("Klaar!")
